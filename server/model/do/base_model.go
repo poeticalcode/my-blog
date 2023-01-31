b8d8cfb13741951c032d1a2e4fa693b3c8017bcf
he.wenyao <he.wenyao@qq.com>
2023-02-01 00:16:08 +0800

    first:增加删除接口

--- server/model/do/base_model.go
+++ server/model/do/base_model.go
@@ -6,7 +6,6 @@ import (
 )
 
 type baseModel struct {
-	id        gorm.DeletedAt
 	Id        int            `json:"id" gorm:"primaryKey"`
 	CreatedAt time.Time      `json:"created_at" gorm:"autoCreateTime"` // 创建时间
 	UpdatedAt time.Time      `json:"updated_at" gorm:"autoUpdateTime"` // 更新时间
