3903d0e01ccf02bd77d7d311d30df96cdc20cfe5
he.wenyao <he.wenyao@qq.com>
2023-02-08 18:44:10 +0800

    fix:增加注释

--- server/model/entity/entity.go
+++ server/model/entity/entity.go
@@ -6,7 +6,7 @@ import (
 	"gorm.io/gorm"
 )
 
-// 基础模型 [公共部分]
+// BaseModel 基础模型 [公共部分]
 type BaseModel struct {
 	ID        uint           `json:"id" gorm:"primaryKey"`
 	CreatedAt time.Time      `json:"created_at"` // 创建时间
@@ -14,7 +14,7 @@ type BaseModel struct {
 	DeletedAt gorm.DeletedAt `json:"-"`          // 删除时间
 }
 
-// 文章模型
+// Article 文章模型
 type Article struct {
 	BaseModel
 	Title       string `json:"title"`       // 文章标题
@@ -23,16 +23,18 @@ type Article struct {
 	Description string `json:"description"` // 文章描述
 }
 
+// TableName 返回 Article 模型对应的数据库表名
 func (Article) TableName() string {
 	return "tb_article"
 }
 
-// 标签模型
+// Tag 标签模型
 type Tag struct {
 	BaseModel
 	Name string `json:"name"` // 标签名称
 }
 
+// TableName 返回 Tag 模型对应的数据库表名
 func (Tag) TableName() string {
 	return "tb_tag"
 }
