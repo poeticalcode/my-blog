0383b524f7ee78f557797e4bcd58c6bd607b8ba1
he.wenyao <he.wenyao@qq.com>
2023-02-10 23:31:19 +0800

    d

--- web/client/src/views/home/Index.vue
+++ web/client/src/views/home/Index.vue
@@ -1,23 +1,27 @@
 <template>
+
+
   <el-row :gutter="10">
-    <el-col :xs="8" :sm="6" :md="4" :lg="18" :xl="12" :offset="6">
+
+    <el-col :xs="8" :sm="6" :md="4" :lg="14" :xl="12" :offset="1">
       <!-- 下拉刷新 -->
-      <div v-infinite-scroll="initTableData">
+      <!-- <div v-infinite-scroll="initTableData"> -->
         <!-- 渲染文章 -->
-        <el-card :body-style="{ padding: '0px' }" v-for="item in articleList" :key="item.id" @click="toArticleDetail(item.id)">
+        <el-card style="margin: 16px 0;" :body-style="{ padding: '0px' }" v-for="item in articleList" :key="item.id"
+          @click="toArticleDetail(item.id)">
           <div class="card-inner">
             <!-- 封面 -->
-            <el-image style=" height: 240px" :src="item.cover" lazy />
+            <el-image style="width: 360px; " :src="item.cover" lazy fit="fill" />
             <!-- 文章信息 -->
-            <div style="padding: 14px">
+            <div style="padding: 14px;flex: 1;height: 220px;">
               <p> {{ item.description }}</p>
             </div>
           </div>
         </el-card>
-      </div>
+      <!-- </div> -->
     </el-col>
 
- <!--    <el-col :xs="8" :sm="6" :md="4" :lg="6" :xl="6">
+    <!--    <el-col :xs="8" :sm="6" :md="4" :lg="6" :xl="6">
       <div class="grid-content ep-bg-purple-light" />
     </el-col> -->
   </el-row>
@@ -31,13 +35,13 @@ import { ref, reactive, watch } from "vue";
 let articleList = ref([])
 
 let startNum = 1
-let pageSize = 1
+let pageSize = 10
 let currentPage = -1
 let totalPage = 0
 
 // 初始化表格数据
 const initTableData = async () => {
-  if (currentPage === totalPage || totalPage > 0) {
+  if (currentPage === totalPage && totalPage > 0) {
     console.log("已经到底");
     return
   }
@@ -55,11 +59,11 @@ const initTableData = async () => {
     return
   }
 };
-
+initTableData()
 
 // 去文章详情页面
-const toArticleDetail = (id)=>{
-  window.open("/#/article?id="+id)
+const toArticleDetail = (id) => {
+  window.open("/#/article?id=" + id)
 }
 
 </script>
@@ -72,12 +76,16 @@ const toArticleDetail = (id)=>{
   list-style: none;
 }
 
+
+.el-card .card-inner {
+  display: flex;
+  height: 220px;
+}
+
 .el-card {
   cursor: pointer;
 }
-.el-card .card-inner {
-  display: flex;
-}
+
 
 .main-inner {
   display: flex;
