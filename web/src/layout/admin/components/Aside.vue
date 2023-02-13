183da71b55366e39d2abc82ef742091eb70018c5
he.wenyao <he.wenyao@qq.com>
2023-02-13 14:58:25 +0800

    合并 client 以及 admin

---
+++ web/src/layout/admin/components/Aside.vue
@@ -0,0 +1,66 @@
+<template>
+  <el-menu
+      default-active="2"
+      :router="true"
+      background-color="rgb(58, 118, 191)"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+      :collapse="layoutConfig.isCollapse"
+  >
+
+    <el-menu-item index="/admin/">
+      <el-icon>
+        <HomeFilled/>
+      </el-icon>
+      <template #title>首页</template>
+    </el-menu-item>
+
+    <el-sub-menu index="/admin/article">
+      <template #title>
+        <el-icon>
+          <Document/>
+        </el-icon>
+        <span>博文管理</span>
+      </template>
+      <el-menu-item index="/admin/article/list">博文列表</el-menu-item>
+      <el-menu-item index="/admin/article/post">发表文章</el-menu-item>
+    </el-sub-menu>
+
+    <el-menu-item index="3">
+      <el-icon>
+        <Picture/>
+      </el-icon>
+      <template #title>图片管理</template>
+    </el-menu-item>
+
+    <el-menu-item index="4">
+      <el-icon>
+        <Tools/>
+      </el-icon>
+      <template #title>系统管理</template>
+    </el-menu-item>
+
+  </el-menu>
+
+</template>
+
+<script setup>
+import {inject} from "vue";
+
+const layoutConfig = inject("layoutConfig")
+
+</script>
+
+<style scoped>
+
+.el-menu {
+  height: 100%;
+}
+
+/*菜单未折叠的宽度 */
+.el-menu:not(.el-menu--collapse) {
+  width: 200px;
+}
+
+
+</style>
---
+++ web/src/router/index.js
@@ -0,0 +1,112 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import { findLast } from "lodash"
+import { check, isLogin } from "../util/auth.js"
+// 路由映射表
+const routes = [
+    {
+        path: '/admin',
+        component: () => import(/*webpackChunkName:"layout"*/ "@/layout/admin/Layout.vue"),
+        children: [
+            {
+                path: '/',
+                component: () => import(/*webpackChunkName:"index"*/ "@/views/admin/Index.vue"),
+                // 任何人都可以阅读文章
+                meta: {
+                    title: '首页'
+                }
+            },
+            {
+                path: '/article/list',
+                component: () => import(/*webpackChunkName:"admin-article"*/ "@/views/admin/aritcle/ArticleList.vue"),
+                // 任何人都可以阅读文章
+                meta: {
+                    title: '文章列表'
+                }
+            },
+            {
+                path: '/article/post',
+                component: () => import(/*webpackChunkName:"admin-article"*/ "@/views/admin/aritcle/PostArticle.vue"),
+                // 任何人都可以阅读文章
+                meta: {
+                    title: '图片列表'
+                }
+            }
+        ]
+    },
+    {
+        path: '/',
+        component: () => import(/*webpackChunkName:"layout"*/ "@/layout/Layout.vue"),
+        children: [
+            {
+                path: '/',
+                component: () => import(/*webpackChunkName:"home"*/ "@/views/home/Index.vue"),
+                // 任何人都可以阅读文章
+                meta: {
+                    title: '首页',
+                }
+            },
+            {
+                path: '/article',
+                component: () => import(/*webpackChunkName:"article"*/ "@/views/article/Index.vue"),
+                // 任何人都可以阅读文章
+                meta: {
+                    title: '文章详情',
+                }
+            },
+            {
+                path: '/about',
+                component: () => import(/*webpackChunkName:"about"*/ "@/views/about/Index.vue"),
+                // 任何人都可以阅读文章
+                meta: {
+                    title: '关于我',
+                }
+            },
+            {
+                path: '/archive',
+                component: () => import(/*webpackChunkName:"archive"*/ "@/views/archive/Index.vue"),
+                // 任何人都可以阅读文章
+                meta: {
+                    title: '归档',
+                }
+            },
+            {
+                path: '/tags',
+                component: () => import(/*webpackChunkName:"tags"*/ "@/views/tags/Index.vue"),
+                // 任何人都可以阅读文章
+                meta: {
+                    title: '标签',
+                }
+            }
+        ]
+    },
+    {
+        path: '/:pathMatch(.*)',
+        component: () => import(/*webpackChunkName:"home"*/ "@/views/404/Index.vue"),
+        // 404 页面
+        meta: {
+            title: '404',
+        }
+    }
+]
+
+// 创建一个路由
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+router.beforeEach(async (to, from, next) => {
+    if (to.meta.title) {
+        document.title = to.meta.title
+    }
+    next()
+})
+
+router.afterEach(() => {
+    NProgress.done()
+})
+
+// 导出一个路由
+export default router;
