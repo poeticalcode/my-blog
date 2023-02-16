52e6237179a6806322d53b833cff01af8ec25694
he.wenyao <he.wenyao@qq.com>
2023-02-16 12:47:21 +0800

    fix

--- web/src/layout/components/Header.vue
+++ web/src/layout/components/Header.vue
@@ -1,29 +1,26 @@
 <template>
   <div class="nav">
-    <div>
+    <div class="brand" @click="router.push('/')">
       <span>He'Blog</span>
     </div>
-    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" :ellipsis="false"
-      @select="handleSelect" :router="true">
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" :ellipsis="false" :router="true">
       <el-menu-item index="/">首页</el-menu-item>
       <el-menu-item index="/archive">归档</el-menu-item>
       <el-menu-item index="/tags">标签</el-menu-item>
       <el-menu-item index="/about">关于我</el-menu-item>
     </el-menu>
-  </div>
+</div>
 </template>
 
 <script setup>
 
 import { ref } from 'vue'
-
+import { useRouter } from 'vue-router'
 const activeIndex = ref('/')
-const handleSelect = (key, keyPath) => {
-  console.log(key, keyPath)
-}
+const router = useRouter()
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .el-menu {
   border-bottom: unset;
 }
@@ -32,5 +29,9 @@ const handleSelect = (key, keyPath) => {
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  .brand {
+    cursor: pointer;
+  }
 }
 </style>
