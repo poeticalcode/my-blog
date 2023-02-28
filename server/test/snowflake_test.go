d2d215537be184686a1fff0ff44701af979ddee2
he.wenyao <he.wenyao@qq.com>
2023-02-28 15:07:13 +0800

    fix

--- server/test/snowflake_test.go
+++ server/test/snowflake_test.go
@@ -3,6 +3,8 @@ package test
 import (
 	"testing"
 
+	"github.com/he-wen-yao/my-blog/server/db"
+	"github.com/he-wen-yao/my-blog/server/global"
 	"github.com/he-wen-yao/my-blog/server/util/snowflake"
 )
 
@@ -10,4 +12,16 @@ func Test_Snowflake(t *testing.T) {
 	snow := snowflake.NewSnowflake(0, 0)
 	snowflakeId := snow.NextVal()
 	t.Log(snowflakeId)
+}
+
+func Test_FlushArticleId(t *testing.T) {
+	snow := snowflake.NewSnowflake(0, 0)
+	result := []int64{}
+	global.DB = db.InitDB()
+	global.DB.Table("tb_article").Select("id").Find(&result)
+	for _, id := range result {
+		snowflakeId := snow.NextVal()
+		global.DB.Table("tb_article").Where("id = ?", id).UpdateColumn("id", snowflakeId)
+		t.Log(snowflakeId)
+	}
 }
