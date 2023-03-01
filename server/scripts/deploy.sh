32494c606df64f77b0743c9573b5cb7a085b3cb7
he.wenyao <he.wenyao@qq.com>
2023-03-01 13:55:00 +0800

    fix

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -1,1 +1,1 @@
-docker build -f Dockerfile -t my-blog-docker:latest ../build/
+git pull && docker build -f ../build/Dockerfile -t my-blog-docker:latest . && docker stop blog-server && docker rm blog-server && docker run -d -p 9090:9090 --name blog-server my-blog-docker
