c7cc4e16fcbb726d54f51d71a7ae1a172a091be0
he.wenyao <he.wenyao@qq.com>
2023-03-02 15:40:39 +0800

    fix

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -46,28 +46,29 @@ func main() {
 	global.DB = db.DB
 	global.Snowflake = snowflake.NewSnowflake(0, 0)
 	app := initialize.RouterInit()
+    // 定义一个 http 服务
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", c.Port),
 		Handler: app,
 	}
-
+    // 用协程启动 http 服务
 	go func() {
 		// 服务连接
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
 		}
 	}()
-
-	// 等待中断信号以优雅地关闭服务器（设置 5 秒的超时时间）
+	// 定义一个 chan 监听终端信号等待中断信号
 	quit := make(chan os.Signal)
 	signal.Notify(quit, os.Interrupt)
 	<-quit
 	log.Println("Shutdown Server ...")
-
+    //（设置 5 秒的超时时间）
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+    // 关闭 http 服务，停止接收请求，等待 5s，处理剩余请求
 	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("Server Shutdown:", err)
+		log.Fatal("Server Shutdown: ", err)
 	}
 	log.Println("Server exiting")
 
