1fd05f4492939344ee7cc676387ff2bee16a3383
he.wenyao <he.wenyao@qq.com>
2023-03-05 23:30:36 +0800

    first: 修改 DbUInt64 类型转 JSON 问题

--- server/model/entity/str_int64.go
+++ server/model/entity/str_int64.go
@@ -19,6 +19,6 @@ func (d *DbUInt64) UnmarshalJSON(data []byte) (err error) {
 }
 
 // MarshalJSON 这里需要手动加上引号，不会自动生成
-func (d *DbUInt64) MarshalJSON() ([]byte, error) {
-	return ([]byte)("\"" + strconv.FormatInt(int64(*d), 10) + "\""), nil
+func (d DbUInt64) MarshalJSON() ([]byte, error) {
+	return ([]byte)("\"" + strconv.FormatInt(int64(d), 10) + "\""), nil
 }
